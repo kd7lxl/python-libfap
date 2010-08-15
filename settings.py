3147403480b2fdf559039a6db24c33860e4102ba
Tom Hayward <esarfl+github@gmail.com>
2010-08-15 15:02:24 -0700

    settings.py: Add error check for APRS_USER and APRS_PASSCODE.

--- settings.py
+++ settings.py
@@ -2,3 +2,7 @@ APRS_SERVER_HOST = 'rotate.aprs2.net'
 APRS_SERVER_PORT = 14580
 APRS_USER = ''
 APRS_PASSCODE = ''
+
+
+# Check that APRS_USER and APRS_PASSCODE are set
+assert len(APRS_USER) > 3 and len(APRS_PASSCODE) > 0, 'Please set APRS_USER and APRS_PASSCODE in settings.py.'
